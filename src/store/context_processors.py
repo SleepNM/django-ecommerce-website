9112a99a4e65d3e391097debae3298a5b85cd7e3
SleepNoMore <nninkovic.py@gmail.com>
2020-09-16 05:35:19 +0200

    Finished payment logic for logged in users

--- src/store/context_processors.py
+++ src/store/context_processors.py
@@ -3,9 +3,12 @@ from .models import Order
 
 def total_items(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
-        order= Order.objects.get(customer=customer, complete=False)
-        cartItems = order.get_cart_items
+        try:
+            customer = request.user.customer
+            order = Order.objects.get(customer=customer, complete=False)
+            cartItems = order.get_cart_items
+        except:
+            cartItems = 0
     else:
         cartItems = 404 #Placeholder for not logged in user
     return {'cartItems':cartItems}
--- src/store/views.py
+++ src/store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+import datetime
 from .models import *
 
 
@@ -63,5 +64,28 @@ def update_item(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
-    print("Data:", request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print("User is not logged in")
     return JsonResponse('Payment complete!', safe=False)
